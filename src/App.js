fdbe3c43c62d3c159c2d50eb1461b34fca773e84
Dhafer Alzubairi <dx.dhafer@gmail.com>
2022-03-26 01:42:51 +0200

    increment decrement buttons functionality implemented

--- src/App.js
+++ src/App.js
@@ -30,7 +30,293 @@ class App extends PureComponent {
 				cartOverlay: 'inactive'
 			},
 			chosenAttributes: [],
-			cart: [],
+			cart: [
+				{
+					"name": "iMac 2021",
+					"brand": "Apple",
+					"prices": [
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "USD",
+								"symbol": "$"
+							},
+							"amount": 1688.03
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "GBP",
+								"symbol": "£"
+							},
+							"amount": 1213.34
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "AUD",
+								"symbol": "A$"
+							},
+							"amount": 2177.57
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "JPY",
+								"symbol": "¥"
+							},
+							"amount": 182294.51
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "RUB",
+								"symbol": "₽"
+							},
+							"amount": 127653.82
+						}
+					],
+					"id": "apple-imac-2021",
+					"attributes": [
+						[
+							{
+								"id": "apple-imac-2021",
+								"name": "Capacity",
+								"value": "512GB"
+							},
+							{
+								"id": "apple-imac-2021",
+								"name": "With USB 3 ports",
+								"value": "No"
+							},
+							{
+								"id": "apple-imac-2021",
+								"name": "Touch ID in keyboard",
+								"value": "No"
+							}
+						]
+					],
+					"quantity": 1,
+					"gallery": [
+						"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac-24-blue-selection-hero-202104?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1617492405000"
+					]
+				},
+				{
+					"name": "Jacket",
+					"brand": "Canada Goose",
+					"prices": [
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "USD",
+								"symbol": "$"
+							},
+							"amount": 518.47
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "GBP",
+								"symbol": "£"
+							},
+							"amount": 372.67
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "AUD",
+								"symbol": "A$"
+							},
+							"amount": 668.83
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "JPY",
+								"symbol": "¥"
+							},
+							"amount": 55990.46
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "RUB",
+								"symbol": "₽"
+							},
+							"amount": 39207.96
+						}
+					],
+					"id": "jacket-canada-goosee",
+					"attributes": [
+						[
+							{
+								"id": "jacket-canada-goosee",
+								"name": "Size",
+								"value": "M"
+							}
+						]
+					],
+					"quantity": 1,
+					"gallery": [
+						"https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016105/product-image/2409L_61.jpg",
+						"https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016107/product-image/2409L_61_a.jpg",
+						"https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016108/product-image/2409L_61_b.jpg",
+						"https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016109/product-image/2409L_61_c.jpg",
+						"https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016110/product-image/2409L_61_d.jpg",
+						"https://images.canadagoose.com/image/upload/w_1333,c_scale,f_auto,q_auto:best/v1634058169/product-image/2409L_61_o.png",
+						"https://images.canadagoose.com/image/upload/w_1333,c_scale,f_auto,q_auto:best/v1634058159/product-image/2409L_61_p.png"
+					]
+				},
+				{
+					"name": "Nike Air Huarache Le",
+					"brand": "Nike x Stussy",
+					"prices": [
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "USD",
+								"symbol": "$"
+							},
+							"amount": 144.69
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "GBP",
+								"symbol": "£"
+							},
+							"amount": 104
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "AUD",
+								"symbol": "A$"
+							},
+							"amount": 186.65
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "JPY",
+								"symbol": "¥"
+							},
+							"amount": 15625.24
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "RUB",
+								"symbol": "₽"
+							},
+							"amount": 10941.76
+						}
+					],
+					"id": "huarache-x-stussy-le",
+					"attributes": [
+						[
+							{
+								"id": "huarache-x-stussy-le",
+								"name": "Size",
+								"value": "43"
+							}
+						]
+					],
+					"quantity": 1,
+					"gallery": [
+						"https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087",
+						"https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_1_720x.jpg?v=1612816087",
+						"https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_3_720x.jpg?v=1612816087",
+						"https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_5_720x.jpg?v=1612816087",
+						"https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_4_720x.jpg?v=1612816087"
+					]
+				},
+				{
+					"name": "iPhone 12 Pro",
+					"brand": "Apple",
+					"prices": [
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "USD",
+								"symbol": "$"
+							},
+							"amount": 1000.76
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "GBP",
+								"symbol": "£"
+							},
+							"amount": 719.34
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "AUD",
+								"symbol": "A$"
+							},
+							"amount": 1290.99
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "JPY",
+								"symbol": "¥"
+							},
+							"amount": 108074.6
+						},
+						{
+							"__typename": "Price",
+							"currency": {
+								"__typename": "Currency",
+								"label": "RUB",
+								"symbol": "₽"
+							},
+							"amount": 75680.48
+						}
+					],
+					"id": "apple-iphone-12-pro",
+					"attributes": [
+						[
+							{
+								"id": "apple-iphone-12-pro",
+								"name": "Capacity",
+								"value": "512G"
+							},
+							{
+								"id": "apple-iphone-12-pro",
+								"name": "Color",
+								"value": "#03FFF7"
+							}
+						]
+					],
+					"quantity": 1,
+					"gallery": [
+						"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;.v=1604021663000"
+					]
+				}
+			],
 		};
 	}
 
@@ -265,7 +551,6 @@ class App extends PureComponent {
 		const { dropdown } = this.state;
 		const cartId = 'navbar-cart';
 		const currencyId = 'navbar-currency';
-		// console.log(id);
 
 		if (id === cartId && dropdown.cartOverlay === 'inactive') {
 			this.setState({
@@ -292,37 +577,76 @@ class App extends PureComponent {
 				}
 			});
 
-		};
+		} else
+			if (id === currencyId && dropdown.currency === 'active') {
+				this.setState({
+					dropdown: {
+						currency: 'inactive',
+						cartOverlay: 'inactive'
+					}
+				});
+
+			}
 
 	};
 
-	handleIncrementDecrement = (e) => {
-		// console.log(e.target.name, 'name');
-		// console.log(e.target.id, 'id');
+	handleIncrementDecrement = (e, item) => {
 		const id = e.target.id;
 		const name = e.target.name;
 
-		const matchingItem = this.state.cart.filter(item =>
+		const matchingItemIndex = this.state.cart.findIndex(item =>
 			item.id === id
 		);
+		const incremented = {
+			name: item.name,
+			brand: item.brand,
+			prices: item.prices,
+			id: item.id,
+			quantity: item.quantity + 1,
+			gallery: item.gallery
+		};
+		const decremented = {
+			name: item.name,
+			brand: item.brand,
+			prices: item.prices,
+			id: item.id,
+			quantity: item.quantity - 1,
+			gallery: item.gallery
+		};
 
-		// console.log(matchingItem, 'matchingItem');
-
-
+		const newCart = this.state.cart.filter(item =>
+			item.id !== id
+		);
+		if (name === 'increment') {
+			newCart.splice(matchingItemIndex, 0, incremented);
+			this.setState({
+				cart: newCart
+			});
+		}
+		if (name === 'decrement') {
+			newCart.splice(matchingItemIndex, 0, decremented);
+			this.setState({
+				cart: newCart
+			});
+		}
 	};
 
 	//to hide dropdown and cart overlay when clicking anywhere else on the page
 	resetter = (e) => {
 		const id = e.target.id;
+		const name = e.target.name;
 		const cartId = 'navbar-cart';
 		const currencyId = 'navbar-currency';
-		if (id !== cartId && id !== currencyId) {
+		// console.log(e);
+		//exceptions are 
+		if (id !== cartId && name !== 'increment' && name !== 'decrement' && id !== currencyId && id !== currencyId && id !== 'cart-overlay') {
 			this.setState({
 				dropdown: {
 					currency: 'inactive',
 					cartOverlay: 'inactive'
 				}
 			});
+
 		}
 	};
 
--- src/components/CartOverlay/CartOverlay.Component.jsx
+++ src/components/CartOverlay/CartOverlay.Component.jsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import './CartOverlay.styles.scss';
-import { myCart } from './myCart';
 
 class CartOverlay extends PureComponent {
 	constructor(props) {
@@ -8,92 +7,133 @@ class CartOverlay extends PureComponent {
 	}
 
 	render() {
-		const { cart, currency, handleIncrementDecrement, dropdown } = this.props;
-		const carty = myCart;
+		const {
+			cart,
+			currency,
+			handleIncrementDecrement,
+			dropdown,
+			handleClicksForDropDown,
+		} = this.props;
 		// console.log(dropdown);
 
 		return (
 			<div
+				onClick={handleClicksForDropDown}
 				id='cart-overlay'
 				className={
 					dropdown === 'active' ? 'cart-overlay' : 'cart-overlay-invisible'
 				}>
-				<h2 className='cart-overlay-title'>
+				<h2 id='cart-overlay' className='cart-overlay-title'>
 					my Bag{' '}
-					<span className='cart-overlay-products-count'>
+					<span id='cart-overlay' className='cart-overlay-products-count'>
 						{`${cart.length ? cart.length : 'no'}`} items
 					</span>
 				</h2>
 				<div
+					id='cart-overlay'
 					className={
 						cart.length <= 1
 							? 'cart-overlay-items-main-container-mini'
 							: 'cart-overlay-items-main-container'
 					}>
 					{cart.length > 0
-						? cart.map((item, i) => (
-								<div key={i} className={'cart-overlay-item-container'}>
-									{' '}
-									<div className='cart-overlay-item-left-container'>
-										<h3 className='cart-overlay-item-title'>
-											{item.brand}
-											<br />
-											{item.name}
-										</h3>
-										{currency.length > 0
-											? item.prices.map(
-													(price, i) =>
-														price.currency.label === currency[0].label && (
-															<p className='cart-overlay-item-price' key={i}>
-																{`${price.currency.symbol}${price.amount}`}
-															</p>
-														),
-											  )
-											: null}
-										<div className='cart-overlay-item-attributes-container'>
-											<div className='cart-overlay-item-attribute-box'>s</div>
-											<div className='cart-overlay-item-attribute-box'>s</div>
-										</div>
-									</div>
-									<div className='cart-overlay-item-right-container'>
-										<div className='cart-overlay-item-mid-container'>
-											<button
-												id={item.id}
-												name='increment'
-												onClick={handleIncrementDecrement}
-												className='cart-overlay-item-attribute-increment-decrement'>
-												+
-											</button>
+						? cart.map(
+								(item, i) =>
+									item.quantity >= 1 && (
+										<div
+											id='cart-overlay'
+											key={i}
+											className={'cart-overlay-item-container'}>
+											{' '}
+											<div
+												id='cart-overlay'
+												className='cart-overlay-item-left-container'>
+												<h3
+													id='cart-overlay'
+													className='cart-overlay-item-title'>
+													{item.brand}
+													<br />
+													{item.name}
+												</h3>
+												{currency.length > 0
+													? item.prices.map(
+															(price, i) =>
+																price.currency.label === currency[0].label && (
+																	<p
+																		id='cart-overlay'
+																		className='cart-overlay-item-price'
+																		key={i}>
+																		{`${price.currency.symbol}${price.amount}`}
+																	</p>
+																),
+													  )
+													: null}
+												<div
+													id='cart-overlay'
+													className='cart-overlay-item-attributes-container'>
+													<div className='cart-overlay-item-attribute-box'>
+														s
+													</div>
+													<div
+														id='cart-overlay'
+														className='cart-overlay-item-attribute-box'>
+														s
+													</div>
+												</div>
+											</div>
+											<div
+												id='cart-overlay'
+												className='cart-overlay-item-right-container'>
+												<div className='cart-overlay-item-mid-container'>
+													<button
+														id={item.id}
+														name='increment'
+														onClick={(e) => handleIncrementDecrement(e, item)}
+														className='cart-overlay-item-attribute-increment-decrement'>
+														+
+													</button>
 
-											<p className='cart-overlay-item-quantity'>
-												{item.quantity}
-											</p>
-											<button
-												id={item.id}
-												name='decrement'
-												onClick={handleIncrementDecrement}
-												className='cart-overlay-item-attribute-increment-decrement'>
-												-
-											</button>
+													<p
+														id='cart-overlay'
+														className='cart-overlay-item-quantity'>
+														{item.quantity}
+													</p>
+													<button
+														id={item.id}
+														name='decrement'
+														onClick={(e) => handleIncrementDecrement(e, item)}
+														className='cart-overlay-item-attribute-increment-decrement'>
+														-
+													</button>
+												</div>
+												<img
+													id='cart-overlay'
+													src={item.gallery[0]}
+													className='cart-overlay-item-img'
+													alt={item.name}
+												/>
+											</div>
 										</div>
-										<img
-											src={item.gallery[0]}
-											className='cart-overlay-item-img'
-											alt={item.name}
-										/>
-									</div>
-								</div>
-						  ))
+									),
+						  )
 						: null}
 				</div>
 
-				<div className='cart-overlay-total-price-container'>
-					<div className='cart-overlay-total-price-text'>total</div>
-					<div className='cart-overlay-total-price-amount'>$1329</div>
+				<div id='cart-overlay' className='cart-overlay-total-price-container'>
+					<div id='cart-overlay' className='cart-overlay-total-price-text'>
+						total
+					</div>
+					<div id='cart-overlay' className='cart-overlay-total-price-amount'>
+						$1329
+					</div>
 				</div>
-				<div className='cart-overlay-total-price-container'>
-					<button className='cart-overlay-view-btn'>view bag</button>
-					<button className='cart-overlay-checkout-btn'>checkout</button>
+				<div id='cart-overlay' className='cart-overlay-total-price-container'>
+					<button id='cart-overlay' className='cart-overlay-view-btn'>
+						view bag
+					</button>
+					<button id='cart-overlay' className='cart-overlay-checkout-btn'>
+						checkout
+					</button>
 				</div>
 			</div>
 		);
