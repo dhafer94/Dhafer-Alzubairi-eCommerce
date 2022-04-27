94434519a543a044f3edc23b06b490a6d6e1cccf
Dhafer Alzubairi <dx.dhafer@gmail.com>
2022-04-27 10:54:44 +0200

    Added heroku url for backend and deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Product from './pages/Product/Product.Component';
 import Cart from './pages/Cart/Cart.Component';
@@ -11,7 +10,7 @@ import Category from './pages/Category/Category.Component';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/',
+	uri: 'https://dhafer-e-commerce-react-app.herokuapp.com',
 	cache: new InMemoryCache(),
 });
 
@@ -31,8 +30,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);
